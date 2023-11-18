bf839dd51eabb89eb841072ce8bf31a4bdf7070a
chp320 <chp320@naver.com>
2023-11-19 02:59:28 +0900

    JPA(H2, in-memory DB)를 활용한 데이터 조회 api 테스트

---
+++ src/main/java/com/skyfox83/springbootdeveloper/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.skyfox83.springbootdeveloper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
